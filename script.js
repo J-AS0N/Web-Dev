f8b807ade6e4ecadcb40d6ef7d15a9c96e85a1f1
Jason <jasonweng44@gmail.com>
2024-06-14 20:59:37 -0400

    a function that adds elements to a list

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+let add = document.getElementById("addButton");
+let del = document.getElementById("deleteButton");
+
+add.onclick = function () {
+    // selects the first ordered list
+    let ol = document.querySelector('ol');
+
+    // create a new element <li> </li> 
+    let li = document.createElement('li');
+
+    // appends the element <li> </li> onto the ordered list
+    ol.append(li)
+
+    // adds the text to the list
+    li.innerText = 'Lucifer';
+};
+
+del.onclick = function () {
+    
+
+};
